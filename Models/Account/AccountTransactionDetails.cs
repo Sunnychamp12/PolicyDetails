8bdd0a04979e1bd9cce9abc600fe4f1e880f0f1b
sunny champ <sunnychamp12@outlook.com>
2023-09-15 14:13:39 +0530

    Update Account Transaction Class

--- Models/Account/AccountTransactionDetails.cs
+++ Models/Account/AccountTransactionDetails.cs
@@ -3,7 +3,6 @@
     public class AccountTransactionDetails
     {
         public int TransactionId { get; set; }
-        public Nullable<int> AccountNo { get; set; }
         public Nullable<System.DateTime> TransDate { get; set; }
         public string Details { get; set; }
         public Nullable<decimal> Debit { get; set; }
